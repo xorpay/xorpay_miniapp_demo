dbc06a7cfb43aeddbc063be8bea80adb6a9ae11c
XorPay 个人支付平台 <i@xorpay.com>
2019-06-10 13:50:49 +0800

    update jump appid

---
+++ xpay_mini_demo/pages/index/index.js
@@ -0,0 +1,54 @@
+const md5 = require('../../utils/md5.js');
+const app = getApp()
+
+Page({
+  data: {
+    'aid': '1',
+    'name': 'XorPay测试',
+    'pay_type': 'jsapi',
+    'price': '0.03',
+    'order_id': 'test-0001',
+    'notify_url': 'https://abc.com/',
+    'more': 'ojbk',
+    'secret': 'xxxx',
+  },
+  //事件处理函数
+  input_text: function (e) {
+    var d = {};
+    d[e.target.id] = e.detail.value;
+    this.setData(d);
+    console.log(this.data);
+  },
+  pay: function() {
+    wx.navigateToMiniProgram({
+      //appId: 'wx6c2423928b92ca14',
+      appId: 'wx6eeed4ca124a1abf',
+      path: 'pages/index/index',
+      extraData: {
+        'aid': this.data.aid,
+        'name': this.data.name,
+        'pay_type': this.data.pay_type,
+        'price': this.data.price,
+        'order_id': this.data.order_id,
+        'notify_url': this.data.notify_url,
+        'more': this.data.more,
+        'sign': md5.md5(this.data.name + this.data.pay_type + this.data.price + this.data.order_id + this.data.notify_url + this.data.secret),
+      },
+      envVersion: 'develop',
+      fail(res) {
+        wx.showToast({
+          title: res.errMsg,
+          icon: 'none',
+        });
+      },
+      success(res) {
+        wx.showToast({
+          title: '跳转成功',
+          icon: 'none',
+        });
+      },
+    });
+  },
+  onLoad: function () {
+  },
+})
