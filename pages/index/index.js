97bd05af635e047dea8bbdf566264adc8556cf80
XorPay 个人支付平台 <i@xorpay.com>
2019-06-10 13:47:54 +0800

    update jump appid

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,7 +21,7 @@ Page({
   },
   pay: function() {
     wx.navigateToMiniProgram({
-      appId: 'wx6c2423928b92ca14',
+      appId: 'wx6eeed4ca124a1abf',
       path: 'pages/index/index',
       extraData: {
         'aid': this.data.aid,
